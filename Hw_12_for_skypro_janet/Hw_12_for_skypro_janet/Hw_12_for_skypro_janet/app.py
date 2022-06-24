e69d614d1bff0600c84999c2577903891051afb2
Dzhanet <104907844+Dzhanet@users.noreply.github.com>
2022-06-24 23:14:44 +0200

    Поправлен логер, добавлена обработка ошибок

---
+++ Hw_12_for_skypro_janet/Hw_12_for_skypro_janet/Hw_12_for_skypro_janet/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from main.views import main_blueprint
+from loader.views import loader_blueprint
+import logging
+
+# Импортируем блюпринты из их пакетов
+
+app = Flask(__name__)
+
+import logger
+
+#конфигурация
+app.config['POST_PATH'] = "posts.json"
+app.config['UPLOAD_FOLDER'] = "uploads/images"
+
+# Регистрируем блюпринты
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Hw_12_for_skypro_janet/Hw_12_for_skypro_janet/Hw_12_for_skypro_janet/main/views.py
@@ -0,0 +1,24 @@
+# Сперва импорттируем класс блюпринта
+from flask import Blueprint, render_template, request
+from functions import search_posts
+import logging
+from werkzeug.exceptions import NotFound
+# Затем создаем новый блюпринт, выбираем для него имя
+main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates') #добавим настройку папки с шаблонами
+logger = logging.getLogger('basiq')
+# Создаем вьюшку, используя в декораторе блюпринт вместо app
+@main_blueprint.route('/')
+def page_index():
+
+    return render_template("index.html")
+
+@main_blueprint.route('/search/')
+def page_tag():
+    s = request.args.get('s') #получаем данные из адресной строки (переменную, переда-ю из index.html в адр методом гет
+    if s:
+        posts_with_tag = search_posts(s) #получаем список словарей с постами по тегу от юзера
+        return render_template("post_list.html", s=s, items=posts_with_tag) #items-словарь, к-й пребирает в шаблоне
+    else:
+        logger.info('Пользователь ничего не ввел')
+        raise NotFound
+
